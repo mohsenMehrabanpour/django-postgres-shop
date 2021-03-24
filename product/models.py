bd2f45f721ac3a6f2de176c9964d860fe049d6c1
mohsen <flash2000w@gmail.com>
2021-03-25 02:49:17 +0430

    Add Product & Image & Category models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    price = models.IntegerField(_('price'))
+    description = models.TextField(_('description'), blank=True)
+    created_at = models.DateTimeField(_('created_at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+        db_table = 'product'
+
+
+class Image(models.Model):
+    image = models.ImageField(_('image'), upload_to='products/image')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='image')
+    created_at = models.DateTimeField(_('created_at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+
+    class Meta:
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
+        db_table = 'image'
+
+
+class Category(models.Model):
+    title = models.CharField(_('title'),max_length=255)
+    product = models.ManyToManyField(Product)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        db_table = 'category'
